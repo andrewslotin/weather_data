3623d99ab6844bb106a16129b1f9edbef72fa0fd
Andrew Slotin <andrew.slotin@gmail.com>
2013-03-18 18:20:37 +0100

    Basic conversions

--- spec/weather_data/temperature/base_spec.rb
+++ spec/weather_data/temperature/base_spec.rb
@@ -11,6 +11,14 @@ describe WeatherData::Temperature::Base do
     end
   end
 
+  describe "#==" do
+    it "can be compared to Numeric" do
+      expect(subject.new(10) == 10).to be_true
+      expect(subject.new(10) == 10.0).to be_true
+      expect(subject.new(10) == 11).to be_false
+    end
+  end
+
   describe "#to_i" do
     it "returns degrees rounded" do
       expect(subject.new(15.6).to_i).to eq 16
